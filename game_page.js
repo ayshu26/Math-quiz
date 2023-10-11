5ae8950963ca1b82f634c5f86db1696ab294fbb1
ayshu26 <126812520+ayshu26@users.noreply.github.com>
2023-10-11 19:38:01 +0100

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,14 @@
+function send(){
+    
+}
+p1name=localStorage.getItem("player1name")
+p2name=localStorage.getItem("player2name")
+p1score=0 
+p2score=0
+
+
+
+document.getElementById("p1name").innerHTML=p1name
+document.getElementById("p2name").innerHTML=p2name
+document.getElementById("p1score").innerHTML=p1score
+document.getElementById("p2score").innerHTML=p2score
